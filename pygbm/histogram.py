a55be13a071f7d6116c9f1da8d516436f0bddd59
Olivier Grisel <olivier.grisel@ensta.org>
2018-08-06 00:21:11 +0200

    Compute loss gain on split

--- pygbm/histogram.py
+++ pygbm/histogram.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numba import njit
+from numba import float32, uint8
 
 
 HISTOGRAM_DTYPE = np.dtype([
@@ -56,3 +57,45 @@ def build_histogram_unrolled(n_bins, sample_indices, binned_feature,
         histogram[bin_idx].count += 1
 
     return histogram
+
+
+@njit(fastmath=True)
+def _split_gain(gradient_left, hessian_left, gradient_right, hessian_right,
+                gradient_parent, hessian_parent, l2_regularization):
+    """Loss reduction
+
+    Compute the reduction in loss after taking a split compared to keeping
+    the node a leaf of the tree.
+
+    See Equation 7 of:
+    XGBoost: A Scalable Tree Boosting System, T. Chen, C. Guestrin, 2016
+    https://arxiv.org/abs/1603.02754
+    """
+    def negative_loss(gradient, hessian):
+        return gradient ** 2 / (hessian + l2_regularization)
+
+    gain = negative_loss(gradient_left, hessian_left)
+    gain += negative_loss(gradient_right, hessian_right)
+    gain -= negative_loss(gradient_parent, hessian_parent)
+    return gain
+
+
+@njit(locals={'gradient_left': float32, 'hessian_left': float32,
+              'best_gain': float32, 'best_bin_idx': uint8},
+      fastmath=True)
+def find_split(histogram, gradient_parent, hessian_parent, l2_regularization):
+    gradient_left, hessian_left, best_gain = 0., 0., 0.
+    best_bin_idx = 0
+    for bin_idx in range(histogram.shape[0]):
+        gradient_left += histogram[bin_idx].sum_gradients
+        hessian_left += histogram[bin_idx].sum_hessians
+        gradient_right = gradient_parent - gradient_left
+        hessian_right = hessian_parent - hessian_left
+        gain = _split_gain(gradient_left, hessian_left,
+                           gradient_right, hessian_right,
+                           gradient_parent, hessian_parent,
+                           l2_regularization)
+        if gain > best_gain:
+            best_gain = gain
+            best_bin_idx = bin_idx
+    return (best_bin_idx, best_gain)
--- tests/test_histogram.py
+++ tests/test_histogram.py
@@ -3,21 +3,20 @@ import pytest
 from numpy.testing import assert_allclose
 from numpy.testing import assert_array_equal
 from pygbm.histogram import build_histogram, build_histogram_unrolled
+from pygbm.histogram import find_split
 
 
 @pytest.mark.parametrize(
     'build_func', [build_histogram, build_histogram_unrolled])
 def test_build_histogram(build_func):
-    binned_features = np.array(
-        [0, 2, 0, 1, 2, 0, 2, 1],
-        dtype=np.uint8)
+    binned_feature = np.array([0, 2, 0, 1, 2, 0, 2, 1], dtype=np.uint8)
 
     # Small sample_indices (below unrolling threshold)
     ordered_gradients = np.array([0, 1, 3], dtype=np.float32)
     ordered_hessians = np.array([1, 1, 2], dtype=np.float32)
 
     sample_indices = np.array([0, 2, 3], dtype=np.uint32)
-    hist = build_func(3, sample_indices, binned_features,
+    hist = build_func(3, sample_indices, binned_feature,
                       ordered_gradients, ordered_hessians)
     assert_array_equal(hist['count'], [2, 1, 0])
     assert_allclose(hist['sum_gradients'], [1, 3, 0])
@@ -28,8 +27,23 @@ def test_build_histogram(build_func):
     ordered_gradients = np.array([0, 1, 3, 0, 1], dtype=np.float32)
     ordered_hessians = np.array([1, 1, 2, 1, 0], dtype=np.float32)
 
-    hist = build_func(3, sample_indices, binned_features,
+    hist = build_func(3, sample_indices, binned_feature,
                       ordered_gradients, ordered_hessians)
     assert_array_equal(hist['count'], [2, 2, 1])
     assert_allclose(hist['sum_gradients'], [1, 4, 0])
     assert_allclose(hist['sum_hessians'], [2, 2, 1])
+
+
+def test_find_split():
+    binned_feature = np.array([0, 2, 0, 1, 2, 0, 2, 1], dtype=np.uint8)
+    ordered_gradients = np.array([-2, 1, -2, -1, 1, -2, 1, -1],
+                                 dtype=np.float32)
+    ordered_hessians = np.ones_like(binned_feature, dtype=np.float32)
+    sample_indices = np.arange(binned_feature.shape[0], dtype=np.uint32)
+    histogram = build_histogram(3, sample_indices, binned_feature,
+                                ordered_gradients, ordered_hessians)
+    l2_regularization = 0.0001
+    bin_idx, gain = find_split(histogram, ordered_gradients.sum(),
+                               ordered_gradients.sum(), l2_regularization)
+    assert bin_idx == 1
+    assert gain > 0.
