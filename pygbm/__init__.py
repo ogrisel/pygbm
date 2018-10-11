b6596c90d825dc489f40e265b547c8a3321622f0
Olivier Grisel <olivier.grisel@ensta.org>
2018-10-11 16:37:18 +0200

    Disable workaround for numba threading backend fix

--- pygbm/__init__.py
+++ pygbm/__init__.py
@@ -1,9 +1,5 @@
 from pygbm.gradient_boosting import GradientBoostingMachine
 
 
-# Workaround: https://github.com/numba/numba/issues/3341
-import numba
-numba.config.THREADING_LAYER = 'workqueue'
-
 __version__ = '0.1.0.dev0'
 __all__ = ['GradientBoostingMachine']
