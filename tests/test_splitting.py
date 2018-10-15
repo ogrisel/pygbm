89e4b2985384bad5375928d82189bc68630230ad
Nicolas Hug <contact@nicolas-hug.com>
2018-10-15 17:45:48 -0400

    Test for comparing classical and subtraction splitting methods

--- tests/test_splitting.py
+++ tests/test_splitting.py
@@ -1,5 +1,6 @@
 import numpy as np
-from numpy.testing import assert_allclose
+from numpy.testing import assert_almost_equal
+from numpy.testing import assert_array_almost_equal
 import pytest
 
 from pygbm.splitting import _find_histogram_split
@@ -34,34 +35,29 @@ def test_hitstogram_split(n_bins):
             assert split_info.feature_idx == feature_idx
 
 
-@pytest.mark.skip()
-# this is failing at the gain comparison. Investigating why... Maybe its
-# because of the slight differences in the histograms but I doubt it
-def test_split_vs_split_subtraction():
-    # Make sure  find_node_split and finde_node_split_subtraction return the
+@pytest.mark.parametrize('constant_hessian', [True, False])
+def test_split_vs_split_subtraction(constant_hessian):
+    # Make sure find_node_split and find_node_split_subtraction return the
     # same results.
-    # Should we add test about computation time to make sure
+    # Should we add a test about computation time to make sure
     # time(subtraction) < time(regular)?
     rng = np.random.RandomState(42)
 
     n_bins = 10
     n_features = 20
-    n_samples = int(1e3)
+    n_samples = 500
     l2_regularization = 0.
     min_hessian_to_split = 1e-3
-    from pygbm import binning
-    from sklearn.datasets import make_classification
-    #binned_features = rng.randint(0, n_bins - 1, size=n_samples,
-    #dtype=np.uint8)
-    # TODO: change that
-    X, y = make_classification(n_samples=n_samples, random_state=rng)
-    binned_features = binning.BinMapper().fit_transform(X)
+
+    binned_features = rng.randint(0, n_bins, size=(n_samples, n_features),
+                                  dtype=np.uint8)
+    binned_features = np.asfortranarray(binned_features)
     sample_indices = np.arange(n_samples, dtype=np.uint32)
-
     all_gradients = rng.randn(n_samples).astype(np.float32)
-
-    all_hessians = np.ones(n_samples, dtype=np.float32)
-    all_hessians = rng.lognormal(size=n_samples).astype(np.float32)
+    if constant_hessian:
+        all_hessians = np.ones(1, dtype=np.float32)
+    else:
+        all_hessians = rng.lognormal(size=n_samples).astype(np.float32)
 
     splitter = HistogramSplitter(n_features, binned_features, n_bins,
                                  all_gradients, all_hessians,
@@ -71,26 +67,38 @@ def test_split_vs_split_subtraction():
     sample_indices_left = sample_indices[mask]
     sample_indices_right = sample_indices[~mask]
 
-    split_info_parent, hists_parent = splitter.find_node_split(sample_indices)
-    split_info_left, hists_left = splitter.find_node_split(sample_indices_left)
-    split_info_right, hists_right = splitter.find_node_split(sample_indices_right)
+    # first split parent, left and right with classical method
+    si_parent, hists_parent = splitter.find_node_split(sample_indices)
+    si_left, hists_left = splitter.find_node_split(sample_indices_left)
+    si_right, hists_right = splitter.find_node_split(sample_indices_right)
 
-    split_info_left_sub, hists_left_sub = splitter.find_node_split_subtraction(sample_indices_left, hists_parent, hists_right)
-    split_info_right_sub, hists_right_sub = splitter.find_node_split_subtraction(sample_indices_right, hists_parent, hists_left)
-    
+    # split left with subtraction method
+    gradient_left = si_left.gradient_left + si_left.gradient_right
+    hessian_left = si_left.hessian_left + si_left.hessian_right
+    si_left_sub, hists_left_sub = splitter.find_node_split_subtraction(
+        sample_indices_left, hists_parent, hists_right, gradient_left,
+        hessian_left)
+
+    # split right with subtraction method
+    gradient_right = si_right.gradient_left + si_right.gradient_right
+    hessian_right = si_right.hessian_left + si_right.hessian_right
+    si_right_sub, hists_right_sub = splitter.find_node_split_subtraction(
+        sample_indices_right, hists_parent, hists_left, gradient_right,
+        hessian_right)
+
+    # make sure histograms from classical and subtraction method are the same
     for hists, hists_sub in ((hists_left, hists_left_sub),
                              (hists_right, hists_right_sub)):
         for hist, hist_sub in zip(hists, hists_sub):
             for key in ('count', 'sum_hessians', 'sum_gradients'):
-                assert_allclose(hist[key], hist_sub[key], rtol=1e-3)
+                assert_array_almost_equal(hist[key], hist_sub[key], decimal=4)
 
-    
-    for split_info, split_info_sub in (
-            (split_info_left, split_info_left_sub),
-            (split_info_right, split_info_right_sub)):
-        assert split_info.gain == split_info_sub.gain
-        assert split_info.feature_idx == split_info_sub.feature_idx
-        assert split_info.gradient_left == split_info_sub.gradient_left
-        assert split_info.gradient_right == split_info_sub.gradient_right
-        assert split_info.hessian_right == split_info_sub.hessian_right
-        assert split_info.hessian_left == split_info_sub.hessian_left
+    # make sure split_infos from classical and subtraction method are the same
+    for si, si_sub in ((si_left, si_left_sub), (si_right, si_right_sub)):
+        assert_almost_equal(si.gain, si_sub.gain, decimal=4)
+        assert_almost_equal(si.feature_idx, si_sub.feature_idx, decimal=4)
+        assert_almost_equal(si.gradient_left, si_sub.gradient_left, decimal=4)
+        assert_almost_equal(si.gradient_right, si_sub.gradient_right,
+                            decimal=4)
+        assert_almost_equal(si.hessian_right, si_sub.hessian_right, decimal=4)
+        assert_almost_equal(si.hessian_left, si_sub.hessian_left, decimal=4)
