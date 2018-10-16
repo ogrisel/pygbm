a199acbb24cf5ddce32a7a92b51f07734eaa8431
Nicolas Hug <contact@nicolas-hug.com>
2018-10-16 12:02:27 -0400

    added sanity checks for gradients and hessians

--- tests/test_splitting.py
+++ tests/test_splitting.py
@@ -102,3 +102,108 @@ def test_split_vs_split_subtraction(constant_hessian):
                             decimal=4)
         assert_almost_equal(si.hessian_right, si_sub.hessian_right, decimal=4)
         assert_almost_equal(si.hessian_left, si_sub.hessian_left, decimal=4)
+
+
+@pytest.mark.parametrize('constant_hessian', [True, False])
+def test_gradient_and_hessian_sanity(constant_hessian):
+    # This test checks that the values of gradients and hessians are
+    # consistent in different places:
+    # - in split_info: si.gradient_left + si.gradient_right must be equal to
+    #   the gradient at the node. Same for hessians.
+    # - in the histograms: summing 'sum_gradients' over the bins must constant
+    #   across all features, and those sums must be equal to the node's
+    #   gradient. Same for hessians.
+    #
+    # These checks are carried out for split_info and histograms resulting
+    # from both find_node_split() and find_node_split_subtraction().
+    #
+    # The structure of this test is exactly the same as in
+    #   test_split_vs_split_subtraction() but it's probably best to keep them
+    #   separate, after all their not checking the same things.
+
+    rng = np.random.RandomState(42)
+
+    n_bins = 10
+    n_features = 20
+    n_samples = 500
+    l2_regularization = 0.
+    min_hessian_to_split = 1e-3
+
+    binned_features = rng.randint(0, n_bins, size=(n_samples, n_features),
+                                  dtype=np.uint8)
+    binned_features = np.asfortranarray(binned_features)
+    sample_indices = np.arange(n_samples, dtype=np.uint32)
+    all_gradients = rng.randn(n_samples).astype(np.float32)
+    if constant_hessian:
+        all_hessians = np.ones(1, dtype=np.float32)
+    else:
+        all_hessians = rng.lognormal(size=n_samples).astype(np.float32)
+
+    splitter = HistogramSplitter(n_features, binned_features, n_bins,
+                                 all_gradients, all_hessians,
+                                 l2_regularization, min_hessian_to_split)
+
+    mask = rng.randint(0, 2, n_samples).astype(np.bool)
+    sample_indices_left = sample_indices[mask]
+    sample_indices_right = sample_indices[~mask]
+
+    # first split parent, left and right with classical method
+    si_parent, hists_parent = splitter.find_node_split(sample_indices)
+    si_left, hists_left = splitter.find_node_split(sample_indices_left)
+    si_right, hists_right = splitter.find_node_split(sample_indices_right)
+
+    # split left with subtraction method
+    gradient_left = si_left.gradient_left + si_left.gradient_right
+    hessian_left = si_left.hessian_left + si_left.hessian_right
+    si_left_sub, hists_left_sub = splitter.find_node_split_subtraction(
+        sample_indices_left, hists_parent, hists_right, gradient_left,
+        hessian_left)
+
+    # split right with subtraction method
+    gradient_right = si_right.gradient_left + si_right.gradient_right
+    hessian_right = si_right.hessian_left + si_right.hessian_right
+    si_right_sub, hists_right_sub = splitter.find_node_split_subtraction(
+        sample_indices_right, hists_parent, hists_left, gradient_right,
+        hessian_right)
+
+    # make sure that si.gradient_left + si.gradient_right have their expected
+    # value, same for hessians
+    for si, indices in (
+            (si_parent, sample_indices),
+            (si_left, sample_indices_left),
+            (si_left_sub, sample_indices_left),
+            (si_right, sample_indices_right),
+            (si_right_sub, sample_indices_right)):
+        gradient = si.gradient_right + si.gradient_left
+        expected_gradient = all_gradients[indices].sum()
+        hessian = si.hessian_right + si.hessian_left
+        if constant_hessian:
+            expected_hessian = indices.shape[0] * all_hessians[0]
+        else:
+            expected_hessian = all_hessians[indices].sum()
+
+        assert_almost_equal(gradient, expected_gradient, decimal=3)
+        assert_almost_equal(hessian, expected_hessian, decimal=3)
+
+    # make sure sum of gradients in histograms are the same for all features,
+    # and make sure they're equal to their expected value
+    for hists, indices in (
+            (hists_parent, sample_indices),
+            (hists_left, sample_indices_left),
+            (hists_left_sub, sample_indices_left),
+            (hists_right, sample_indices_right),
+            (hists_right_sub, sample_indices_right)):
+        # note: gradients and hessians have shape (n_features,),
+        # we're comparing them to *scalars*. This has the benefit of also
+        # making sure that all the entries are equal.
+        gradients = hists['sum_gradients'].sum(axis=1)  # shape = (n_features,)
+        expected_gradient = all_gradients[indices].sum()  # scalar
+        hessians = hists['sum_hessians'].sum(axis=1)
+        if constant_hessian:
+            # 0 is not the actual hessian, but it's not computed in this case
+            expected_hessian = 0.
+        else:
+            expected_hessian = all_hessians[indices].sum()
+
+        assert_almost_equal(gradients, expected_gradient, decimal=4)
+        assert_almost_equal(hessians, expected_hessian, decimal=4)
