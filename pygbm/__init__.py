a9b3fa7b937bbb6f676ce7aecf609596365c95ab
Nicolas Hug <contact@nicolas-hug.com>
2018-12-14 18:58:11 -0500

    set version to 0.1.0

--- pygbm/__init__.py
+++ pygbm/__init__.py
@@ -3,6 +3,6 @@ from pygbm.gradient_boosting import GradientBoostingRegressor
 from pygbm.gradient_boosting import BaseGradientBoostingMachine
 
 
-__version__ = '0.1.0.dev0'
+__version__ = '0.1.0'
 __all__ = ['BaseGradientBoostingMachine', 'GradientBoostingClassifier',
            'GradientBoostingRegressor']
